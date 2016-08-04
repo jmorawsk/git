3dcdf6ff3028a93a7123d4c4b384c191c2bd34d6
jmorawsk <jmorawsk@ualberta.ca>
2016-08-04 11:03:26 -0600

    Added median tool

--- CoCo/src/helpers/ListHelper.java
+++ CoCo/src/helpers/ListHelper.java
@@ -12,4 +12,24 @@ public class ListHelper {
 
 		return sum / list.size();
 	}
+	
+public static Double median(ArrayList<Double> list) {
+		
+		Double sum = 0.0;
+
+		if(list.size()%2 == 0){
+			if(list.isEmpty()){
+				return Double.NaN;
+			}
+
+			int index = list.size()/2;
+			
+			sum = (list.get(index)+list.get(index-1)) / 2;
+		}
+		else {
+			sum = list.get((list.size()-1)/2);
+		}
+
+		return sum;
+	}
 }
